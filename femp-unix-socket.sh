3990fada5f4cf5c9b9ec6b6ef9a77ad10291e0af
Albert Valbuena <46286613+Adminbyaccident@users.noreply.github.com>
2023-01-15 11:39:22 +0100

    Update femp-unix-socket.sh
    Corrected php-fpm service entry

--- femp-unix-socket.sh
+++ femp-unix-socket.sh
@@ -69,7 +69,8 @@ sed -i -e '/listen.group/s/;listen.group/listen.group/' /usr/local/etc/php-fpm.d
 sed -i -e '/listen.mode/s/;listen.mode/listen.mode/' /usr/local/etc/php-fpm.d/www.conf
 
 # Enable PHP-FPM at boot time
-php_fpm_enable="YES"
+sysrc php_fpm_enable="YES"
+service php-fpm start
 
 # Install MySQL 8.0
 pkg install -y mysql80-server
